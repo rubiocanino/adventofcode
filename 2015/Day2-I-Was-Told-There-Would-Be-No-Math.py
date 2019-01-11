9fd213266787ee94628f2b9c779f6b792473bca2
Saul Rubio <saulrubio98@hotmail.com>
2019-01-10 21:47:47 -0600

    Day 2 - Part 2

--- 2015/Day2-I-Was-Told-There-Would-Be-No-Math.py
+++ 2015/Day2-I-Was-Told-There-Would-Be-No-Math.py
@@ -1,4 +1,5 @@
 import time
+import math
 # Get the sizes of the boxes
 print("---------------------------------------------------")
 # You only need to change the variable for the inputs
@@ -12,17 +13,28 @@ time.sleep(1)
 # To split the string in an array of chars 
 sizesArray = list(sizes)
 totalArea = []
+totalRibbon = []
 for i in sizesArray:
     splitSizes = i.split('x')
     splitSizes = map(int, splitSizes)
     # The formula given by the problem 2*l*w + 2*w*h + 2*h*l
     area = (2*splitSizes[0]*splitSizes[1]) + (2*splitSizes[1]*splitSizes[2]) + (2*splitSizes[2]*splitSizes[0])
+    # Calculate the needed ribbon
+    ribbonBox = splitSizes[0] * splitSizes[1] * splitSizes[2]
     # Sorting the array to find the smallest sizes of the box
     splitSizes = sorted(splitSizes)
+    # Calculate the needed ribbon
+    ribbonBox += (splitSizes[0] + splitSizes[0]) + (splitSizes[1] + splitSizes[1]) 
+    
     # Adding the extra papper
     area+= splitSizes[0]*splitSizes[1]
     # Adding to an array that saves all the square feets needed
     totalArea.append(area)
+    # print ribbonBox
+    totalRibbon.append(ribbonBox)
+
 
 # Concatenate and sum all the array of the square feet needed
 print('The elves need to order:' + str(sum(totalArea))+ ' of wrap paper')
+
+print('The elves need to order:' + str(sum(totalRibbon))+ ' of ribbon')
